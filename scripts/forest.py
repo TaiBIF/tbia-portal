99afa176fd272aec2f928114eb6ff20bd7d5835e
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-08-30 11:06:24 +0800

    add scripts for API testing

--- scripts/forest.py
+++ scripts/forest.py
@@ -1,8 +1,22 @@
 # key a684b9ba-1a1a-49ac-8a9c-7485b6502775
 import requests
 import pandas as pd
+from requests.api import request
 from conf.settings import env
 
-request_url = f"http://ecollect.forest.gov.tw/EcologicalOdata/odata/data?$filter=decimalLatitude gt 22.696100 and decimalLatitude lt 22.696120 and decimalLongitude gt 120.996940 and decimalLongitude lt 120.996945$top=10&key={env('FOREST_KEY')}"
+request_url = f"https://ecollect.forest.gov.tw/EcologicalOdata/odata/data?$filter=vernacularname eq '白羊草'&$top=10"
 response = requests.get(request_url)
 
+# get all plan
+request_url = "https://ecollect.forest.gov.tw/EcologicalOdata/odata/odata_plan_basic"
+response = requests.get(request_url)
+data = response.json()['value'] # 595
+plans = []
+for i in range(len(data)):
+    name = data[i]['plan_name']
+    plans.append(name)
+
+# get all data
+for i in plans:
+    request_url = f"https://ecollect.forest.gov.tw/EcologicalOdata/odata/data?$filter=planname eq '{i}'"
+    response = requests.get(request_url)
---
+++ scripts/tfri.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+"""
+Ecological Metadata Language, or EML, is a metadata standard that records information about ecological datasets in a series of modular and extensible XML document types.
+"""
+
+df = pd.read_table('/Users/taibif/Documents/02-tbia-portal/tfri_herbarium/occurrence.txt')
+
