e5929753855d6799b4835cdc55b0d070c23588dc
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-09-15 23:02:04 +0800

    update data processing steps

--- scripts/tbn.py
+++ scripts/tbn.py
@@ -66,6 +66,11 @@ dataset_uuid = datasets.datasetUUID.tolist()
 dataset_uuid.remove('4caf0c2f-25ad-4898-a1e6-5346457f0233')
 dataset_uuid.remove('7e5a68cc-771e-42ef-8cf7-b68f2254d15b')
 
+# remove already finished dataset
+dataset_uuid.remove('79ae93e0-a2b8-11de-9f79-b8a03c50a862')
+dataset_uuid.sort() # prevent from ebird being firtst one 
+
+
 for i in range(len(dataset_uuid)):
     uuid = dataset_uuid[i]
     dataset_name = datasets[datasets['datasetUUID']==uuid].datasetName.values[0]
@@ -82,96 +87,104 @@ for i in range(len(dataset_uuid)):
         data = response.json()
         total_data += data["data"]
         j += 1
+        if j % 300 == 0:
+            df = pd.DataFrame(total_data)
+            df.to_csv(f"../tbia-volumes/tbn_data/{uuid}_{j}.csv")
+            total_data = []
     df = pd.DataFrame(total_data)
-    df.to_csv(f"../tbia-volumes/tbn_data/{uuid}.csv")
-    for k in range(len(df)):
-        print(f"{dataset_name}, {k}, {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-        row = df.iloc[k]
-        taxon_id, sensitiveState, name_code, precision, data_generalize = None, None, None, row.coordinatePrecision, None
-        # NomenMatch
-        request_url = f"http://match.taibif.tw/api.php?names={row.scientificName}&best=yes&format=json"
-        response = requests.get(request_url)
-        res = response.json()['results'][0][0]
-        if res['best']:
-            if res['best']:
-                taxon_id = res['best'].get('tbn',None)
-                # get sensitive state & taicol name code
-                # !!! need to change if use taicol taxon in the future !!!
-                if taxon_id:
-                    request_url = f"https://www.tbn.org.tw/api/v2/species?uuid={taxon_id}"
-                    response = requests.get(request_url)
-                    t = response.json()['data']
-                    if t:
-                        name_code = t[0]['taicolNameCode']
-                        sensitiveState = t[0]['sensitiveState']
-                        if sensitiveState == '輕度':
-                            precision = 0.01
-                            data_generalize = True
-                        elif sensitiveState == '重度':
-                            precision = 0.1
-                            data_generalize = True
-                        taxon_rank = t[0]['taxonRank']
-                        rank_e = rank[rank['rank_c']==taxon_rank]['rank'].values[0] if taxon_rank in rank.rank_c.to_list() else None
-        # check dataset belongs to occurrence or collection
-        # PreservedSpecimen, FossilSpecimen, LivingSpecimen, MaterialSample, Event, HumanObservation, MachineObservation, Taxon, Occurrence, MaterialCitation
-        if 'Specimen' not in row.basisOfRecord:
-            Occurrence.objects.create(
-                rightsHolder = 'TBN',
-                taxonUUID = taxon_id,
-                originalModified = convert_date(row.modified),
-                originalCreated = convert_date(row.created),
-                occurrenceID = row.occurrenceUUID,
-                originalScientificName = row.scientificName,
-                originalVernacularName = row.vernacularName,
-                sensitiveCategory = sensitiveState,
-                taxonRank = rank_e,
-                eventDate = row.eventDate,
-                standardDate = convert_date(row.eventDate),
-                verbatimLongitude = row.decimalLongitude,
-                verbatimLatitude = row.decimalLatitude,
-                verbatimCoordinateSystem = 'decimalDegrees' if row.decimalLongitude and row.decimalLatitude else None,
-                verbatimSRS = 'WGS84' if row.decimalLongitude and row.decimalLatitude else None,
-                standardLongitude = float(row.decimalLongitude) if row.decimalLongitude not in ['', None] else None,
-                standardLatitude = float(row.decimalLatitude) if row.decimalLatitude not in ['', None] else None,
-                dataGeneralizations = data_generalize,
-                coordinateUncertaintyInMeters = row.coordinateUncertaintyInMeters,
-                coordinatePrecision = precision,
-                locality = row.eventPlaceAdminarea,
-                organismQuantity = row.organismQuantityType if row.individualCount == '' else row.individualCount,
-                organismQuantityType = row.organismQuantityType, 
-                scientificNameID = name_code,
-                basisOfRecord = row.basisOfRecord,
-                datasetName = row.datasetName,
-                resourceContacts = row.datasetAuthor,
-                references = f"https://www.tbn.org.tw/occurrence/{row.tbnID}"
-            )
-        else:
-            Collection.objects.create(
-                occurrenceID = row.occurrenceUUID,
-                rightsHolder = 'TBN',
-                taxonUUID = taxon_id,
-                originalScientificName = row.scientificName,
-                originalVernacularName = row.vernacularName,
-                taxonRank = rank_e,
-                eventDate = row.eventDate,
-                standardDate = convert_date(row.eventDate),
-                verbatimLongitude = row.decimalLongitude,
-                verbatimLatitude = row.decimalLatitude,
-                verbatimCoordinateSystem = 'decimalDegrees' if row.decimalLongitude and row.decimalLatitude else None,
-                verbatimSRS = 'WGS84' if row.decimalLongitude and row.decimalLatitude else None,
-                standardLongitude = float(row.decimalLongitude) if row.decimalLongitude not in ['', None] else None,
-                standardLatitude = float(row.decimalLatitude) if row.decimalLatitude not in ['', None] else None,
-                dataGeneralizations = data_generalize,
-                coordinateUncertaintyInMeters = row.coordinateUncertaintyInMeters,
-                coordinatePrecision = precision,
-                locality = row.eventPlaceAdminarea,
-                organismQuantity = row.organismQuantityType if row.individualCount == '' else row.individualCount,
-                organismQuantityType = row.organismQuantityType, 
-                scientificNameID = name_code,
-                datasetName = row.datasetName,
-                resourceContacts = row.datasetAuthor,
-                references = f"https://www.tbn.org.tw/occurrence/{row.tbnID}",
-                sensitiveCategory = sensitiveState,
-                originalModified = convert_date(row.modified),
-                originalCreated = convert_date(row.created),
-            )
+    df.to_csv(f"../tbia-volumes/tbn_data/{uuid}_{j}.csv")
+
+
+
+
+    # for k in range(len(df)):
+    #     print(f"{dataset_name}, {k}, {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    #     row = df.iloc[k]
+    #     taxon_id, sensitiveState, name_code, precision, data_generalize = None, None, None, row.coordinatePrecision, None
+    #     # NomenMatch
+    #     request_url = f"http://match.taibif.tw/api.php?names={row.scientificName}&best=yes&format=json"
+    #     response = requests.get(request_url)
+    #     res = response.json()['results'][0][0]
+    #     if res['best']:
+    #         if res['best']:
+    #             taxon_id = res['best'].get('tbn',None)
+    #             # get sensitive state & taicol name code
+    #             # !!! need to change if use taicol taxon in the future !!!
+    #             if taxon_id:
+    #                 request_url = f"https://www.tbn.org.tw/api/v2/species?uuid={taxon_id}"
+    #                 response = requests.get(request_url)
+    #                 t = response.json()['data']
+    #                 if t:
+    #                     name_code = t[0]['taicolNameCode']
+    #                     sensitiveState = t[0]['sensitiveState']
+    #                     if sensitiveState == '輕度':
+    #                         precision = 0.01
+    #                         data_generalize = True
+    #                     elif sensitiveState == '重度':
+    #                         precision = 0.1
+    #                         data_generalize = True
+    #                     taxon_rank = t[0]['taxonRank']
+    #                     rank_e = rank[rank['rank_c']==taxon_rank]['rank'].values[0] if taxon_rank in rank.rank_c.to_list() else None
+    #     # check dataset belongs to occurrence or collection
+    #     # PreservedSpecimen, FossilSpecimen, LivingSpecimen, MaterialSample, Event, HumanObservation, MachineObservation, Taxon, Occurrence, MaterialCitation
+    #     if 'Specimen' not in row.basisOfRecord:
+    #         Occurrence.objects.create(
+    #             rightsHolder = 'TBN',
+    #             taxonUUID = taxon_id,
+    #             originalModified = convert_date(row.modified),
+    #             originalCreated = convert_date(row.created),
+    #             occurrenceID = row.occurrenceUUID,
+    #             originalScientificName = row.scientificName,
+    #             originalVernacularName = row.vernacularName,
+    #             sensitiveCategory = sensitiveState,
+    #             taxonRank = rank_e,
+    #             eventDate = row.eventDate,
+    #             standardDate = convert_date(row.eventDate),
+    #             verbatimLongitude = row.decimalLongitude,
+    #             verbatimLatitude = row.decimalLatitude,
+    #             verbatimCoordinateSystem = 'decimalDegrees' if row.decimalLongitude and row.decimalLatitude else None,
+    #             verbatimSRS = 'WGS84' if row.decimalLongitude and row.decimalLatitude else None,
+    #             standardLongitude = float(row.decimalLongitude) if row.decimalLongitude not in ['', None] else None,
+    #             standardLatitude = float(row.decimalLatitude) if row.decimalLatitude not in ['', None] else None,
+    #             dataGeneralizations = data_generalize,
+    #             coordinateUncertaintyInMeters = row.coordinateUncertaintyInMeters,
+    #             coordinatePrecision = precision,
+    #             locality = row.eventPlaceAdminarea,
+    #             organismQuantity = row.organismQuantityType if row.individualCount == '' else row.individualCount,
+    #             organismQuantityType = row.organismQuantityType, 
+    #             scientificNameID = name_code,
+    #             basisOfRecord = row.basisOfRecord,
+    #             datasetName = row.datasetName,
+    #             resourceContacts = row.datasetAuthor,
+    #             references = f"https://www.tbn.org.tw/occurrence/{row.tbnID}"
+    #         )
+    #     else:
+    #         Collection.objects.create(
+    #             occurrenceID = row.occurrenceUUID,
+    #             rightsHolder = 'TBN',
+    #             taxonUUID = taxon_id,
+    #             originalScientificName = row.scientificName,
+    #             originalVernacularName = row.vernacularName,
+    #             taxonRank = rank_e,
+    #             eventDate = row.eventDate,
+    #             standardDate = convert_date(row.eventDate),
+    #             verbatimLongitude = row.decimalLongitude,
+    #             verbatimLatitude = row.decimalLatitude,
+    #             verbatimCoordinateSystem = 'decimalDegrees' if row.decimalLongitude and row.decimalLatitude else None,
+    #             verbatimSRS = 'WGS84' if row.decimalLongitude and row.decimalLatitude else None,
+    #             standardLongitude = float(row.decimalLongitude) if row.decimalLongitude not in ['', None] else None,
+    #             standardLatitude = float(row.decimalLatitude) if row.decimalLatitude not in ['', None] else None,
+    #             dataGeneralizations = data_generalize,
+    #             coordinateUncertaintyInMeters = row.coordinateUncertaintyInMeters,
+    #             coordinatePrecision = precision,
+    #             locality = row.eventPlaceAdminarea,
+    #             organismQuantity = row.organismQuantityType if row.individualCount == '' else row.individualCount,
+    #             organismQuantityType = row.organismQuantityType, 
+    #             scientificNameID = name_code,
+    #             datasetName = row.datasetName,
+    #             resourceContacts = row.datasetAuthor,
+    #             references = f"https://www.tbn.org.tw/occurrence/{row.tbnID}",
+    #             sensitiveCategory = sensitiveState,
+    #             originalModified = convert_date(row.modified),
+    #             originalCreated = convert_date(row.created),
+    #         )
