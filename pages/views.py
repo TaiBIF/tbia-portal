11159253e0108a6f72b036bd08f24ca2301c17eb
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-11-29 16:24:10 +0800

    fix bug of css

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,6 @@ from django.http import request
 from django.shortcuts import render, redirect
 from conf.settings import STATIC_ROOT
 
-def home(request):
-    print(STATIC_ROOT)
+def index(request):
     return render(request,'pages/index.html')
 
