6be80eb1603b9be0171e51051d74eceb2b2266b0
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-12-09 14:41:28 +0800

    update user table name

--- account/models.py
+++ account/models.py
@@ -20,20 +20,24 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password=None, **kwargs):
+    def create_superuser(self, username, password, **kwargs):
         """
         Creates and saves a superuser with the given email (username) and password
         """
-        user = self.create_user(
-            username,
-            password=password,
+        user = self.model(
+            username=self.normalize_email(username),
+            # first_name=kwargs['first_name'],
+            # last_name=kwargs['last_name'],
         )
+        user.set_password(password)
         user.is_superuser = True
+        user.is_email_verified = True
         user.is_staff = True
         user.is_active = True
         user.role = 'de'
         user.save(using=self._db)
         return user
+
 
 
 class User(AbstractUser):
@@ -81,7 +85,7 @@ class User(AbstractUser):
         return self.username
 
     class Meta:
-        db_table = 'user'
+        db_table = 'tbia_user'
 
 
 class Unit(models.Model):
