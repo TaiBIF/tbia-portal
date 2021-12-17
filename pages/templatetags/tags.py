3884d0af26891c41ff9281bbea1383744c2fb8fe
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-12-17 14:22:54 +0800

    change fulltext search to case-insensitive

--- pages/templatetags/tags.py
+++ pages/templatetags/tags.py
@@ -1,10 +1,14 @@
 from django import template
 from django.utils.safestring import mark_safe 
+import re
 
 register = template.Library()
 
-@register.filter
-def highlight(text, keyword):
-    highlighted = text.replace(keyword, '<span class="col_red">{}</span>'.format(keyword))
-    return mark_safe(highlighted)
 
+@register.filter
+def highlight(text, keyword, autoescape=True):
+    # first compile the regex pattern using the keyword
+    pattern = re.compile(re.escape(keyword), re.IGNORECASE)
+    # now replace
+    new_value = pattern.sub('<span class="col_red">\g<0></span>', text)
+    return mark_safe(new_value)
