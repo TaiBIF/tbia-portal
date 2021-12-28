40d36dee5cf6113de305a02c93e1014227853b5b
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-12-28 16:48:01 +0800

    add settings for staging

---
+++ scripts/docker-web-entry.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /code
+
+npm run build
+
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+
+gunicorn --bind 0.0.0.0:8001 --workers=2 --timeout 60 conf.wsgi:application
