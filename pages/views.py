87097c2bc10fdb9665e51261c7426b9f0e9be533
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2021-12-29 16:47:27 +0800

    feature: index stat

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.http import request
 from django.shortcuts import render, redirect
 from .models import *
+from utils.solr_query import SolrQuery
 
 
 def qa(request):
@@ -12,6 +13,19 @@ def index(request):
     # recommended keyword
     keywords = Keyword.objects.filter(displayed=True).values_list('keyword', flat=True)
 
+    # count of data
+    solr = SolrQuery('tbia_occurrence')
+    query_list = [('q', '*:*'),('rows', 0)]
+    req = solr.request(query_list)
+    count_occurrence = req['solr_response']['response']['numFound']
+    count_occurrence = "{:,}".format(count_occurrence)
+
+    solr = SolrQuery('tbia_collection')
+    query_list = [('q', '*:*'),('rows', 0)]
+    req = solr.request(query_list)
+    count_collection = req['solr_response']['response']['numFound']
+    count_collection = "{:,}".format(count_collection)
+
     # resource
     resource = Resource.objects.order_by('-modified')
     resource_rows = []
@@ -22,4 +36,5 @@ def index(request):
             'url': x.url,
             'date': x.modified.strftime("%Y.%m.%d")
         })
-    return render(request, 'pages/index.html', {'resource': resource_rows, 'keywords': keywords})
+    return render(request, 'pages/index.html', {'resource': resource_rows, 'keywords': keywords, 'count_occurrence': count_occurrence, 
+                                                'count_collection': count_collection})
