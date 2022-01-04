5bceb166a43b3693c6281531a97804d690f213b8
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2022-01-04 11:06:39 +0800

    add steps for updating solr

--- scripts/data_prep/update_tbn_solr.py
+++ scripts/data_prep/update_tbn_solr.py
@@ -3,8 +3,10 @@ import pandas as pd
 import os
 import glob
 import requests
+import json
 
-folder = '/tbia-volumes/solr/csvs/col'
+# folder = '/tbia-volumes/solr/csvs/col'
+folder = '/home/centos/tbia-volumes/solr/csvs/col'
 # tbn_path = '/Users/taibif/Documents/GitHub/tbia-volumes/tbn_data'
 extension = 'csv'
 os.chdir(folder)
@@ -15,11 +17,27 @@ for i in range(len_f):
     print(folder, i)
     path = os.path.join(folder, files[i])
     df = pd.read_csv(path)
-    df['id'] = 'tbn_' + df['occurrenceID']
-    df.to_csv(path)
+    # df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    # df.to_csv(path, index=False)
+    # df['id'] = df['tbiaUUID']
+    # df = df.drop(['tbiaUUID'], axis=1)
+    # df['references'] = df['references'].replace('.0', '')
+    # df.to_csv(path, index=False)
+    df['tbnID'] = df.references.apply(lambda x: x.split('/')[-1])
+    for j in df.index:
+        print(j, df.iloc[j].id)
+        request_url = f"https://www.tbn.org.tw/api/v2/occurrence?tbnID={df.iloc[j].tbnID}"
+        response = requests.get(request_url)
+        t = response.json()['data']
+        sensitiveCategory = t[0]['dataSensitiveIndicator']
+        updates = {'id': df.iloc[j].id, 'sensitiveCategory': {'set': sensitiveCategory}}
+        headers = {'Content-Type': 'application/json',}
+        params = (('commit', 'true'),)
+        data = json.dumps([updates])
+        response = requests.post('http://127.0.0.1:8983/solr/tbia_collection/update', headers=headers, params=params, data=data)
 
-
-folder = '/tbia-volumes/solr/csvs/occ'
+# folder = '/tbia-volumes/solr/csvs/occ'
+folder = '/home/centos/tbia-volumes/solr/csvs/occ'
 # tbn_path = '/Users/taibif/Documents/GitHub/tbia-volumes/tbn_data'
 extension = 'csv'
 os.chdir(folder)
@@ -30,24 +48,22 @@ for i in range(len_f):
     print(folder, i)
     path = os.path.join(folder, files[i])
     df = pd.read_csv(path)
-    df['id'] = 'tbn_' + df['occurrenceID']
-    df.to_csv(path)
-
-
-# df['tbnID'] = df.references.apply(lambda x: x.split('/')[-1])
-
-# for i in df.index:
-#     print(i, df.iloc[i].tbiaUUID)
-#     request_url = f"https://www.tbn.org.tw/api/v2/occurrence?tbnID={df.iloc[i].tbnID}"
-#     response = requests.get(request_url)
-#     t = response.json()['data']
-#     sensitiveCategory = t[0]['dataSensitiveIndicator']
-#     occurrenceID = df.iloc[i].occurrenceID
-#     updates = {'id': f'tbn_{occurrenceID}', 'sensitiveCategory': {'set': sensitiveCategory}}
-#     headers = {'Content-Type': 'application/json',}
-#     params = (('commit', 'true'),)
-#     data = json.dumps([updates])
-#     response = requests.post('http://solr:8983/solr/tbia_occurrence/update', headers=headers, params=params, data=data)
+    # df['id'] = df['tbiaUUID']
+    # df = df.drop(['tbiaUUID'], axis=1)
+    # df['references'] = df['references'].replace('.0', '')
+    # df.to_csv(path)
+    df['tbnID'] = df.references.apply(lambda x: x.split('/')[-1])
+    for j in df.index:
+        print(j, df.iloc[j].id)
+        request_url = f"https://www.tbn.org.tw/api/v2/occurrence?tbnID={df.iloc[j].tbnID}"
+        response = requests.get(request_url)
+        t = response.json()['data']
+        sensitiveCategory = t[0]['dataSensitiveIndicator']
+        updates = {'id': df.iloc[j].id, 'sensitiveCategory': {'set': sensitiveCategory}}
+        headers = {'Content-Type': 'application/json',}
+        params = (('commit', 'true'),)
+        data = json.dumps([updates])
+        response = requests.post('http://127.0.0.1:8983/solr/tbia_occurrence/update', headers=headers, params=params, data=data)
 
 
 
