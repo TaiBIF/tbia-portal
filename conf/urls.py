2d743182bc3e6557ff40d61750f40fc28eaddc03
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2022-01-14 17:34:00 +0800

    add robots.txt rule

--- conf/urls.py
+++ conf/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .settings import env
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('', include('data.urls')),
     path('', include('account.urls')),
+]
+
+if env in ['stag']:
+    urlpatterns += [
+        path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
 ]
