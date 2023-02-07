2c30ddfd1d77254c5c6d324d8163c9b1cf8f7398
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2023-02-07 17:04:58 +0800

    fix bug: exception when no data

--- scripts/data_prep/tbn_api_v25.py
+++ scripts/data_prep/tbn_api_v25.py
@@ -1,29 +1,7 @@
 # script for TBN API version 2.5
 
-# test for TBN API
-
-import re
-from numpy import nan
 import requests
 import pandas as pd
-from data.models import *
-from datetime import datetime, tzinfo,timedelta
-from dateutil import parser
-import bson
-import time
-import os
-from conf.settings import env
-
-def convert_date(date):
-    formatted_date = None
-    if date != '' and date is not None:
-        try:
-            formatted_date = parser.parse(date) 
-        except parser._parser.ParserError:
-            formatted_date = datetime.fromtimestamp(int(date))
-        except:
-            formatted_date = None
-    return formatted_date
 
 request_url = "https://www.tbn.org.tw/api/v25/dataset?modified=1000-01-01"
 response = requests.get(request_url)
@@ -45,17 +23,17 @@ for d in df.datasetUUID.to_list():
     request_url = f"https://www.tbn.org.tw/api/v25/occurrence?datasetUUID={d}&limit=1000&selfProduced=y"
     response = requests.get(request_url)
     data = response.json()
-    len_of_data = data['meta']['total'] 
-    j = 0
-    total_data = data["data"]
-    while data['links']['next'] != "":
-        print('get:'+d)
-        request_url = data['links']['next']
-        response = requests.get(request_url)
-        data = response.json()
-        total_data += data["data"]
-        j += 1
-    df = pd.DataFrame(total_data)
-    df.to_csv(f"/tbia-volumes/bucket/tbn_v25/{d}.csv")
+    if len_of_data := data['meta'].get('total'):
+        j = 0
+        total_data = data["data"]
+        while data['links']['next'] != "":
+            print('get:'+d)
+            request_url = data['links']['next']
+            response = requests.get(request_url)
+            data = response.json()
+            total_data += data["data"]
+            j += 1
+        df = pd.DataFrame(total_data)
+        df.to_csv(f"/tbia-volumes/bucket/tbn_v25/{d}.csv")
 
 print('done!')
