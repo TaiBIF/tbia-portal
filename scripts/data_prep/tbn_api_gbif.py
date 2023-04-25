d5c4677ebd647c51996b85b200fc095b297bb03a
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2023-04-25 13:47:16 +0800

    init commit

---
+++ scripts/data_prep/tbn_api_gbif.py
@@ -0,0 +1,63 @@
+# script for GBIF (currently from TBN)
+
+import requests
+import pandas as pd
+
+request_url = "https://www.tbn.org.tw/api/v25/dataset?modified=1000-01-01"
+response = requests.get(request_url)
+data = response.json()
+len_of_data = data['meta']['total'] 
+j = 0
+total_data = data["data"]
+while data['links']['next'] != "":
+    print(j)
+    request_url = data['links']['next']
+    response = requests.get(request_url)
+    data = response.json()
+    total_data += data["data"]
+    j += 1
+
+
+df = pd.DataFrame(total_data)
+
+# subset GBIF 資料集
+df = df[df.datasetDataFrom=='GBIF']
+
+df.to_csv('tbn_gbif.csv')
+
+# 確認publisher
+df.datasetPublisher.unique()
+
+# 夥伴單位 publisher 不排除特生
+'National Taiwan Museum'
+'Taiwan Biodiversity Information Facility (TaiBIF)'
+'Taiwan Endemic Species Research Institute'
+'Taiwan Forestry Bureau'
+'Taiwan Forestry Research Institute'
+df = df[~df.datasetPublisher.isin(['National Taiwan Museum',
+'Taiwan Biodiversity Information Facility (TaiBIF)',
+'Taiwan Forestry Bureau',
+'Taiwan Forestry Research Institute'])]
+
+df = df.reset_index(drop=True)
+
+for d in df.datasetUUID.to_list():
+    print('get:' + d)
+    # 只取自產資料
+    request_url = f"https://www.tbn.org.tw/api/v25/occurrence?datasetUUID={d}&limit=1000"
+    response = requests.get(request_url)
+    data = response.json()
+    if len_of_data := data['meta'].get('total'):
+        j = 0
+        total_data = data["data"]
+        while data['links']['next'] != "":
+            print('get:'+d)
+            request_url = data['links']['next']
+            response = requests.get(request_url)
+            data = response.json()
+            total_data += data["data"]
+            j += 1
+        df = pd.DataFrame(total_data)
+        df.to_csv(f"/tbia-volumes/bucket/gbif_v25/{d}.csv")
+
+# print('done!')
