2718e9286d6c854b20eef8a62e6e301d60851e88
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2023-05-12 10:37:28 +0800

    fix bug: cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+# 0 16 * * * docker exec tbia-web-stag-container /code/cronjob.sh >> /tmp/stat.log 2>&1
 
 python ./manage.py shell < ./scripts/system/check_file_expired.py
-python ./manage.py shell < ./scripts/system/sensitive_request_review
+python ./manage.py shell < ./scripts/system/sensitive_request_review.py
 
