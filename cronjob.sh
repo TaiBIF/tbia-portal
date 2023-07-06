173b21956fd38d83e62fb591a44d34168b4e26c9
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2023-07-06 11:39:39 +0800

    update production settings

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 0 16 * * * docker exec tbia-web-stag-container /code/cronjob.sh >> /tmp/stat.log 2>&1
+# 0 16 * * * docker-compose exec web /code/cronjob.sh >> /tmp/stat.log 2>&1
 
 python ./manage.py shell < ./scripts/system/check_file_expired.py
 python ./manage.py shell < ./scripts/system/sensitive_request_review.py
