d648b3c135a02dbace2aa9240d655167f4e6cb5d
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2023-11-16 12:50:23 +0800

    trans: datepicker

--- static/js/pages/news.js
+++ static/js/pages/news.js
@@ -5,20 +5,8 @@ $( function() {
         changePage($(this).data('page'), $(this).data('type'))
     })
 
-    let date_locale = { days: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
-                    daysShort: ['日', '一', '二', '三', '四', '五', '六'],
-                    daysMin: ['日', '一', '二', '三', '四', '五', '六'],
-                    months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
-                    monthsShort: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
-                    today: '今天',
-                    clear: '重設',
-                    dateFormat: 'yyyy-MM-dd',   
-                    timeFormat: 'HH:mm',
-                    firstDay: 1}
-
     let start_date_picker = new AirDatepicker('#start_date',
         { locale: date_locale});
-
 
     let end_date_picker = new AirDatepicker('#end_date',
         { locale: date_locale });
