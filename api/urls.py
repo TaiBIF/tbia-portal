379425948b92a0f860a69ab1b897c7b4d79572be
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2024-03-28 16:05:33 +0800

    feature: datasetAPI

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('api/doc', views.api_doc, name='api_doc'),
     path('api/v1/occurrence', views.occurrence, name='occurrence'),
+    path('api/v1/dataset', views.dataset, name='dataset'),
 ]
