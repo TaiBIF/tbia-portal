899d262c78f459b2aae04649c76b2f424f20a510
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2024-11-01 12:19:12 +0800

    update: cors

--- conf/settings.py
+++ conf/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'api',
     'ckeditor',
     'ckeditor_uploader',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -96,6 +97,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,6 +128,8 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS')
 
 WSGI_APPLICATION = 'conf.wsgi.application'
 
