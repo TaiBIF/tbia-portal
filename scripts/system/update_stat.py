9c0b5be1ee66b226ee7cca2c26a559d9982c4058
jinyinglee <45194511+jinyinglee@users.noreply.github.com>
2025-12-24 11:02:55 +0800

    fix bug: calculate stat

--- scripts/system/update_stat.py
+++ scripts/system/update_stat.py
@@ -4,32 +4,10 @@ import requests
 from conf.settings import SOLR_PREFIX
 import json
 from manager.models import DataStat, TaxonStat
-
+from data.utils import rights_holder_map
 
 # TODO 這邊要修改成最新的
 year_month='2025-09'
-
-
-rights_holder_map = {
-    'GBIF': 'gbif',
-    '中央研究院生物多樣性中心植物標本資料庫': 'hast',
-    '中央研究院生物多樣性中心動物標本館': 'asiz',
-    '台灣生物多樣性網絡 TBN': 'tbri',
-    '國立臺灣博物館典藏': 'ntm',
-    '林業試驗所昆蟲標本館': 'fact',
-    '林業試驗所植物標本資料庫': 'taif',
-    '河川環境資料庫': 'wra',
-    '濕地環境資料庫': 'nps',
-    '生態調查資料庫系統': 'forest',
-    '臺灣國家公園生物多樣性資料庫': 'nps',
-    '臺灣生物多樣性資訊機構 TaiBIF': 'brcas',
-    '海洋保育資料倉儲系統': 'oca',
-    '科博典藏 (NMNS Collection)': 'nmns',
-    '臺灣魚類資料庫': 'ascdc',
-    '國家海洋資料庫及共享平台': 'namr',
-    '集水區友善環境生態資料庫': 'ardswc',
-    '中油生態地圖': 'cpc',
-}
 
 
 query = { "query": "*:*",
@@ -64,12 +42,19 @@ DataStat.objects.filter(year_month=year_month,type='data').delete()
 for d in stat_rightsHolder:
     if d['val'] in rights_holder_map.keys():
         group = rights_holder_map[d['val']]
-    else:
+        DataStat.objects.create(
+            year_month=year_month,
+            count=d['count'],
+            rights_holder=d['val'],
+            group=group,
+            type='data'
+        )
+    elif d['val'] == 'total':
         group = 'total'
-    DataStat.objects.create(
-        year_month=year_month,
-        count=d['count'],
-        rights_holder=d['val'],
-        group=group,
-        type='data'
-    )
+        DataStat.objects.create(
+            year_month=year_month,
+            count=d['count'],
+            rights_holder=d['val'],
+            group=group,
+            type='data'
+        )
--- scripts/system/update_taxon_stat.py
+++ scripts/system/update_taxon_stat.py
@@ -5,7 +5,7 @@ from manager.models import TaxonStat
 from numpy import nan
 import json
 import math
-# from data.utils import taxon_group_map_c
+from data.utils import rights_holder_map
 from datetime import datetime
 now = datetime.now()
 
@@ -18,27 +18,29 @@ now = datetime.now()
 # 以 group + rightsHolder 為單位
 
 
-rights_holder_map = {
-    'GBIF': 'gbif',
-    '中央研究院生物多樣性中心植物標本資料庫': 'hast',
-    '中央研究院生物多樣性中心動物標本館': 'asiz',
-    '台灣生物多樣性網絡 TBN': 'tbri',
-    '國立臺灣博物館典藏': 'ntm',
-    '林業試驗所昆蟲標本館': 'fact',
-    '林業試驗所植物標本資料庫': 'taif',
-    '河川環境資料庫': 'wra',
-    '濕地環境資料庫': 'nps',
-    '生態調查資料庫系統': 'forest',
-    '臺灣國家公園生物多樣性資料庫': 'nps',
-    '臺灣生物多樣性資訊機構 TaiBIF': 'brcas',
-    '海洋保育資料倉儲系統': 'oca',
-    '科博典藏 (NMNS Collection)': 'nmns',
-    '臺灣魚類資料庫': 'ascdc',
-    '國家海洋資料庫及共享平台': 'namr',
-    '集水區友善環境生態資料庫': 'ardswc',
-    '中油生態地圖': 'cpc',
-    'total': 'total'
-}
+# rights_holder_map = {
+#     'GBIF': 'gbif',
+#     '中央研究院生物多樣性中心植物標本資料庫': 'hast',
+#     '中央研究院生物多樣性中心動物標本館': 'asiz',
+#     '台灣生物多樣性網絡 TBN': 'tbri',
+#     '國立臺灣博物館典藏': 'ntm',
+#     '林業試驗所昆蟲標本館': 'fact',
+#     '林業試驗所植物標本資料庫': 'taif',
+#     '河川環境資料庫': 'wra',
+#     '濕地環境資料庫': 'nps',
+#     '生態調查資料庫系統': 'forest',
+#     '臺灣國家公園生物多樣性資料庫': 'nps',
+#     '臺灣生物多樣性資訊機構 TaiBIF': 'brcas',
+#     '海洋保育資料倉儲系統': 'oca',
+#     '科博典藏 (NMNS Collection)': 'nmns',
+#     '臺灣魚類資料庫': 'ascdc',
+#     '國家海洋資料庫及共享平台': 'namr',
+#     '集水區友善環境生態資料庫': 'ardswc',
+#     '中油生態地圖': 'cpc',
+#     'total': 'total'
+# }
+
+rights_holder_map['total'] = 'total'
 
 
 # # 這邊改用bioGroup
